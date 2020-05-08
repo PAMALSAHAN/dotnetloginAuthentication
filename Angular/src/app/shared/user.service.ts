350d3764ce11e2379497f2f82630c320e8268a6e
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-05-08 19:31:18 +0530

    validation error

--- Angular/src/app/shared/user.service.ts
+++ Angular/src/app/shared/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormBuilder, Validators, FormControl, EmailValidator } from '@angular/forms';
+import { FormBuilder, Validators, FormControl, EmailValidator, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -15,11 +15,25 @@ export class UserService {
     Password :this.fb.group({
       Password:['',[Validators.required,Validators.minLength(4)]],
       ConfirmPassword:['',Validators.required]
-    })
+    },{validators:this.checkPassword})
 
     
   
   });
 
+  checkPassword(fbi:FormGroup){ //fbi kiyanne meke parameter ekak eka 
+    let ConfirmPassword=fbi.get('ConfirmPassword');
+    if (fbi.get('Password').errors==null || 'passwordMismatch' in ConfirmPassword.errors) {
+      //erros deka naththam meke athulata enawa
+      if (fbi.get('Password').value!=ConfirmPassword.value) {
+        ConfirmPassword.setErrors({passwordMismatch:true});
+      }
+      else{
+        ConfirmPassword.setErrors(null);
+      }
+      
+    }
+
+  }
   
 }
